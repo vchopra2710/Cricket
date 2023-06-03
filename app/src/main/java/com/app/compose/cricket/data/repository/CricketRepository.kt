b8aa782390d9d56d2a801af40041eb717681100f
Vibhor Chopra <vchopra.2710@gmail.com>
2023-06-03 10:12:49 +0200

    code formatting

--- app/src/main/java/com/app/compose/cricket/data/repository/CricketRepository.kt
+++ app/src/main/java/com/app/compose/cricket/data/repository/CricketRepository.kt
@@ -12,14 +12,12 @@ import com.app.compose.cricket.data.network.model.seriesdetails.SeriesInfoDto
 import com.app.compose.cricket.domain.model.cricscore.CricketScore
 import com.app.compose.cricket.domain.model.currentmatches.CurrentMatch
 import com.app.compose.cricket.domain.model.series.Series
-import com.app.compose.cricket.domain.model.seriesdetails.SeriesDetail
 import com.app.compose.cricket.domain.model.seriesdetails.SeriesInfo
 import com.app.compose.cricket.domain.repository.ICricketRepository
 import com.app.compose.cricket.utils.readJsonFile
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.request
-import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpMethod
 import kotlinx.serialization.json.Json
 import javax.inject.Inject
@@ -35,83 +33,57 @@ class CricketRepository @Inject constructor(
         explicitNulls = false
     }
 
-    override suspend fun getCurrentMatches(): CurrentMatch {
-        val currentMatchDto: CurrentMatchDto =
-            if (CONNECT_WITH_API) {
-                val response: HttpResponse = client.request(ApiUrls.CURRENT_CRICKET_MATCHES.url) {
-                    method = HttpMethod.Get
-                    url {
-                        parameters.append("apikey", BuildConfig.API_KEY)
-                        parameters.append("offset", "0")
-                    }
-                }
-
-                response.body()
-            } else {
-                val currentMatchesString = context.readJsonFile(id = R.raw.current_match_dto)
-                json.decodeFromString(currentMatchesString)
+    override suspend fun getCurrentMatches(): CurrentMatch = if (CONNECT_WITH_API) {
+        client.request(ApiUrls.CURRENT_CRICKET_MATCHES.url) {
+            method = HttpMethod.Get
+            url {
+                parameters.append("apikey", BuildConfig.API_KEY)
+                parameters.append("offset", "0")
             }
+        }.body<CurrentMatchDto>()
+    } else {
+        val currentMatchesString = context.readJsonFile(id = R.raw.current_match_dto)
+        json.decodeFromString<CurrentMatchDto>(currentMatchesString)
+    }.toDomain()
 
-        return currentMatchDto.toDomain()
-    }
-
-    override suspend fun getSeries(): Series {
-        val seriesDto: SeriesDto =
-            if (CONNECT_WITH_API) {
-                val response: HttpResponse = client.request(ApiUrls.SERIES.url) {
-                    method = HttpMethod.Get
-                    url {
-                        parameters.append("apikey", BuildConfig.API_KEY)
-                        parameters.append("offset", "0")
-                    }
-                }
-
-                response.body()
-            } else {
-                val seriesString = context.readJsonFile(id = R.raw.series_list)
-                json.decodeFromString(seriesString)
+    override suspend fun getSeries(): Series = if (CONNECT_WITH_API) {
+        client.request(ApiUrls.SERIES.url) {
+            method = HttpMethod.Get
+            url {
+                parameters.append("apikey", BuildConfig.API_KEY)
+                parameters.append("offset", "0")
             }
+        }.body<SeriesDto>()
+    } else {
+        val seriesString = context.readJsonFile(id = R.raw.series_list)
+        json.decodeFromString<SeriesDto>(seriesString)
+    }.toDomain()
 
-        return seriesDto.toDomain()
-    }
-
-    override suspend fun getCricketScore(): CricketScore {
-        val cricketScoreDto: CricketScoreDto =
-            if (CONNECT_WITH_API) {
-                val response: HttpResponse = client.request(ApiUrls.CRICKET_SCORE.url) {
-                    method = HttpMethod.Get
-                    url {
-                        parameters.append("apikey", BuildConfig.API_KEY)
-                        parameters.append("offset", "0")
-                    }
-                }
-
-                response.body()
-            } else {
-                val cricketScoreString = context.readJsonFile(id = R.raw.cric_score)
-                json.decodeFromString(cricketScoreString)
+    override suspend fun getCricketScore(): CricketScore = if (CONNECT_WITH_API) {
+        client.request(ApiUrls.CRICKET_SCORE.url) {
+            method = HttpMethod.Get
+            url {
+                parameters.append("apikey", BuildConfig.API_KEY)
+                parameters.append("offset", "0")
             }
+        }.body<CricketScoreDto>()
+    } else {
+        val cricketScoreString = context.readJsonFile(id = R.raw.cric_score)
+        json.decodeFromString<CricketScoreDto>(cricketScoreString)
+    }.toDomain()
 
-        return cricketScoreDto.toDomain()
-    }
-
-    override suspend fun getSeriesDetails(seriesId: String): SeriesInfo {
-        val seriesDetailDto: SeriesInfoDto =
-            if (CONNECT_WITH_API) {
-                val response: HttpResponse = client.request(ApiUrls.SERIES_DETAILS.url) {
-                    method = HttpMethod.Get
-                    url {
-                        parameters.append("apikey", BuildConfig.API_KEY)
-                        parameters.append("id", seriesId)
-                    }
-                }
-
-                response.body()
-            } else {
-                val seriesInfoString = context.readJsonFile(id = R.raw.series_info)
-                json.decodeFromString(seriesInfoString)
+    override suspend fun getSeriesDetails(
+        seriesId: String,
+    ): SeriesInfo = if (CONNECT_WITH_API) {
+        client.request(ApiUrls.SERIES_DETAILS.url) {
+            method = HttpMethod.Get
+            url {
+                parameters.append("apikey", BuildConfig.API_KEY)
+                parameters.append("id", seriesId)
             }
-
-        return seriesDetailDto.toDomain()
-    }
+        }.body<SeriesInfoDto>()
+    } else {
+        val seriesInfoString = context.readJsonFile(id = R.raw.series_info)
+        json.decodeFromString<SeriesInfoDto>(seriesInfoString)
+    }.toDomain()
 }
